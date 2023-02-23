e9b57e029ac0e1e81176e12e8d12c7a799817de5
Emilio Barrientos <ebarrientos@mangochango.com>
2023-02-22 18:10:53 -0600

    remove build

--- build/static/js/9678.8c93b3cc.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_ijm_relevic=self.webpackChunk_ijm_relevic||[]).push([[9678],{19678:function(e,c,i){i.r(c);var u=i(94434);c.default=u.g}}]);
-//# sourceMappingURL=9678.8c93b3cc.chunk.js.map
