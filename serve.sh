01322002307af5091477439d6a17ae512e7a1e69
Emilio Barrientos <ebarrientos@mangochango.com>
2023-02-22 18:03:48 -0600

    changed serve file

--- serve.sh
+++ serve.sh
@@ -1,13 +1,3 @@
-# sudo apt update
-# curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
-# sudo apt -y install nodejs
-# sudo apt -y install gcc g++ make
-# curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-# sudo apt update && sudo apt install yarn
-
 npm install -f
 npm run build
-
-sudo npm install -g serve
-serve -s build -n
+sudo node server.js & > output.txt
