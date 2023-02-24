283b6d15e57498e35198573f04c73823daf45374
Emilio Barrientos <ebarrientos@mangochango.com>
2023-02-23 20:14:30 -0600

    Added ssl support

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const path = require('path');
+const https = require('https');
+const fs = require('fs');
 const app = express();
+
+const key = fs.readFileSync(__dirname + '/ssl/private.pem');
+const cert = fs.readFileSync(__dirname + '/ssl/public.pem');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -8,4 +13,7 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+https.createServer({
+  key,
+  cert,
+}, app).listen(443);
